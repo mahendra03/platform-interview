20eadd0e7ace7e52f9d8d7cd919496e6ae9ac21f
Mahendra Rathore <49598426+mahendra03@users.noreply.github.com>
2022-12-01 14:44:19 +0530

    Update provider.tf

--- tf/provider.tf
+++ tf/provider.tf
@@ -14,3 +14,9 @@ provider "vault" {
   address = "http://localhost:8301"
   token   = "083672fc-4471-4ec4-9b59-a285e463a973"
 }
+
+provider "vault" {
+  alias   = "vault_staging"
+  address = "http://localhost:8301"
+  token   = "083672fc-4471-4ec4-9b59-a285e463a973"
+}
